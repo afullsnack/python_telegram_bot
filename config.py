4e7898bb21819bdd0ad4fc37513d37dd75732b74
sourceKing <miraclef60@gmail.com>
2020-08-06 12:14:11 +0100

    Rearranged environment variables and removed exposed api keys and secretes

--- config.py
+++ config.py
@@ -1,8 +1,4 @@
 #this will be the config file for various environmental varriables
+import os
 
-COINBASE_API_KEY = '7Q9gJYHQblw2638e'
-COINBASE_API_SECRET = '38v8U3GFjksp8EibyQQ4Np0vyFoy9hDs'
-LUNO_API_KEY_ID = 'deead9avzqn9u'
-LUNO_API_KEY_SECRET = 'O0dL5GdZ4GmxreWKbYswH8x5qthFhpUOPAJmOLrJ20c'
-LUNO_FULL_ACCESS_API_KEY_SECRET = 'vlSOTBLhove-zyVxCBcBAHpypf-PfFPbIqeU1H7FCxg'
-DB_PASS = 'skeletonkey200'
+DB_PASS = os.environ['DB_PASS']
